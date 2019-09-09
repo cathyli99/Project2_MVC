45754bffb66225810cb95b6877868df6aa7faa41
Cathy Li <cathyli99nz@gmail.com>
2019-09-09 23:13:58 +1000

    first commit

---
+++ Iters/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Iters.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
