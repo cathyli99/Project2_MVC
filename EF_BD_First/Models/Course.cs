398b145331a7dc7e2b792e840315822e6074938a
Cathy Li <cathyli99nz@gmail.com>
2019-09-11 22:45:49 +1000

    upload EntityFramework Database Firstproject

---
+++ EF_BD_First/Models/Course.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EF_BD_First.Models
+{
+    public partial class Course
+    {
+        public Course()
+        {
+            Enrollment = new HashSet<Enrollment>();
+        }
+
+        public int CourseId { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollment { get; set; }
+    }
+}
---
+++ EF_BD_First/Models/ItersDatabaseFirstContext.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace EF_BD_First.Models
+{
+    public partial class ItersDatabaseFirstContext : DbContext
+    {
+        public ItersDatabaseFirstContext()
+        {
+        }
+
+        public ItersDatabaseFirstContext(DbContextOptions<ItersDatabaseFirstContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Course> Course { get; set; }
+        public virtual DbSet<Enrollment> Enrollment { get; set; }
+        public virtual DbSet<Student> Student { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+               // optionsBuilder.UseSqlServer("Data Source=localhost; Initial Catalog=ItersDatabaseFirst; User Id=sa; Password=p@55w0rD; Persist Security Info=False; MultipleActiveResultSets=True; Connect Timeout=120;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.6-servicing-10079");
+
+            modelBuilder.Entity<Course>(entity =>
+            {
+                entity.Property(e => e.CourseId)
+                    .HasColumnName("CourseID")
+                    .ValueGeneratedNever();
+            });
+
+            modelBuilder.Entity<Enrollment>(entity =>
+            {
+                entity.Property(e => e.EnrollmentId).HasColumnName("EnrollmentID");
+
+                entity.Property(e => e.CourseId).HasColumnName("CourseID");
+
+                entity.Property(e => e.StudentId).HasColumnName("StudentID");
+
+                entity.HasOne(d => d.Course)
+                    .WithMany(p => p.Enrollment)
+                    .HasForeignKey(d => d.CourseId);
+
+                entity.HasOne(d => d.Student)
+                    .WithMany(p => p.Enrollment)
+                    .HasForeignKey(d => d.StudentId);
+            });
+
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("ID");
+            });
+        }
+    }
+}
